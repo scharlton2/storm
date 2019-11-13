fe019f47e6e0b79fb33b8f6d8557db67b4dcecc3
Scott Charlton <charlton@usgs.gov>
2019-11-13 16:15:40 -0700

    removed build directories

--- Source_xsy-64bit/testcodes/funcnoarg/Debug/o_mod.f90
+++
@@ -1,8 +0,0 @@
-        !COMPILER-GENERATED INTERFACE MODULE: Wed Oct 15 14:23:27 2008
-        MODULE O_mod
-          INTERFACE 
-            FUNCTION O RESULT(O_0)
-              INTEGER(KIND=4) :: O_0
-            END FUNCTION O
-          END INTERFACE 
-        END MODULE O_mod
--- Source_xsy-64bit/testcodes/funcnoarg/Debug/om_mod.f90
+++
@@ -1,8 +0,0 @@
-        !COMPILER-GENERATED INTERFACE MODULE: Wed Oct 15 14:24:37 2008
-        MODULE OM_mod
-          INTERFACE 
-            FUNCTION OM RESULT(OM_0)
-              INTEGER(KIND=4) :: OM_0
-            END FUNCTION OM
-          END INTERFACE 
-        END MODULE OM_mod
